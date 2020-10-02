f4c742d5c223158a86fd0182bdb3d0837e6a0cce
Gene <TinderholmGene@gmail.com>
2020-10-01 21:11:43 -0500

    don't delete previous entries in case someone needs a history

--- remember/remember.c
+++ remember/remember.c
@@ -4,11 +4,15 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
+// TODO decide if the bottom two should be flags or keyword args
+// TODO implement remember pop (displays last item but also removes it
+// TODO implement remember help (displays help text)
+
 char* get_dir_path();
 char* get_file_path(char* dir_path, char* file_name);
 void create_dir_if_needed(char* path);
 void get_last_entry(char* file_path);
-void create_entry(char* file_path, int argc, char** argv);
+void create_entry(char* file_path, char* tmp_file_path, int argc, char** argv);
 
 int main (int argc, char** argv) {
     char* dir_path = get_dir_path();
@@ -16,24 +20,33 @@ int main (int argc, char** argv) {
     char* file_path = get_file_path(dir_path, "/notes.txt");
 
     if (argc > 1) {
-        // TODO use temp file and keep history
-        /* char* tmp_file_path = get_file_path(dir_path, "/tmp.txt"); */
-        create_entry(file_path, argc, argv);
+        char* tmp_file_path = get_file_path(dir_path, "/tmp.txt");
+        create_entry(file_path, tmp_file_path, argc, argv);
     } else {
         get_last_entry(file_path);
     }
 }
 
-void create_entry(char* file_path, int argc, char** argv) {
-    FILE* fp = fopen(file_path, "w");
+void create_entry(char* file_path, char* tmp_file_path, int argc, char** argv) {
+    FILE* tmp = fopen(tmp_file_path, "w");
+    FILE* fp = fopen(file_path, "r");
 
-    if (fp == NULL) {
-        fprintf(stderr, "Cannot open reminder file.\n");
+    if (tmp == NULL || fp == NULL) {
+        fprintf(stderr, "Error while opening file.\n");
         return;
     } 
     for (int i = 1; i < argc; ++i) {
-        fprintf(fp, "%s ", argv[i]);
+        fprintf(tmp, "%s ", argv[i]);
     }
+    fputc('\n', tmp);
+
+    int c;
+    while ((c = fgetc(fp)) != EOF) 
+        fputc(c, tmp);
+
+    rename(tmp_file_path, file_path);
+
+    fclose(tmp);
     fclose(fp);
 }
 
@@ -44,9 +57,11 @@ void get_last_entry(char* file_path) {
         fprintf(stderr, "Cannot open reminder file.\n");
         return;
     } 
-    int c;
-    while((c = fgetc(fp)) != EOF) 
+    int c = fgetc(fp);
+    while(c != EOF && c != '\n') {
         putchar(c);
+        c = fgetc(fp);
+    }
     putchar('\n');
 
     fclose(fp);
