9962672921131554767844765b1f3a7705a37664
Gene <TinderholmGene@gmail.com>
2020-10-02 17:35:59 -0500

    parse command line flags

--- remember/remember.c
+++ remember/remember.c
@@ -4,12 +4,19 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-// TODO decide if the bottom two should be flags or keyword args
+// TODO implement obey flags
 // TODO implement remember pop (displays last item but also removes it
 // TODO implement remember help (displays help text)
 // TODO implement display all
 // TODO implement clear history
 
+// flags -p and -c
+struct application_flags {
+    int pop;
+    int clear;
+};
+
+int parse_args(int argc, char** argv, struct application_flags* app_flags);
 char* get_dir_path();
 char* get_file_path(char* dir_path, char* file_name);
 void create_dir_if_needed(char* path);
@@ -17,16 +24,42 @@ void get_last_entry(char* file_path);
 void create_entry(char* file_path, char* tmp_file_path, int argc, char** argv);
 
 int main (int argc, char** argv) {
+    struct application_flags app_flags = {0};
+
+    int useless_args = parse_args(argc, argv, &app_flags);
+
+    argv += useless_args;
+    argc -= useless_args;
+
     char* dir_path = get_dir_path();
     create_dir_if_needed(dir_path);
     char* file_path = get_file_path(dir_path, "/notes.txt");
 
-    if (argc > 1) {
+    if (argc > 0) {
         char* tmp_file_path = get_file_path(dir_path, "/tmp.txt");
         create_entry(file_path, tmp_file_path, argc, argv);
     } else {
         get_last_entry(file_path);
     }
+}
+
+int parse_args(int argc, char** argv, struct application_flags* app_flags) {
+    int i = 1;
+    while (i < argc) {
+        if(argv[i][0] != '-')
+            break;
+        for (int j = 1; j < strlen(argv[i]); ++j) {
+            if (argv[i][j] == 'c')
+                app_flags->clear = 1;
+            if (argv[i][j] == 'p')
+                app_flags->pop = 1;
+        }
+
+        ++i;
+        if(strcmp(argv[i], "--"))
+            break;
+    }
+    return i;
 }
 
 void create_entry(char* file_path, char* tmp_file_path, int argc, char** argv) {
@@ -37,7 +70,7 @@ void create_entry(char* file_path, char* tmp_file_path, int argc, char** argv) {
         fprintf(stderr, "Error while opening file.\n");
         return;
     } 
-    for (int i = 1; i < argc; ++i) {
+    for (int i = 0; i < argc; ++i) {
         fprintf(tmp, "%s ", argv[i]);
     }
     fputc('\n', tmp);
